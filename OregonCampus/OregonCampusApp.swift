9f356fbb09bedd3fc93562596c73bbf17a132fb0
Oscar Epp <crazytrain303@gmail.com>
2024-03-13 15:46:17 -0700

    Initial Commit

---
+++ OregonCampus/OregonCampusApp.swift
@@ -0,0 +1,17 @@
+//
+//  OregonCampusApp.swift
+//  OregonCampus
+//
+//  Created by Oscar Epp on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct OregonCampusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
