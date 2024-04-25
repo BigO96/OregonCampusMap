2c0a1221868fe8368b0306f2b9e5544d6099c100
Oscar Epp <oepp@uoregon.edu>
2024-04-25 14:34:01 -0700

    Revamped atheletics view. Uses live data from GoDucks, got rid of mapview. XCode crash before this commit.

--- SwooshU/Views/AthleticsViews/AthleticsView.swift
+++ SwooshU/Views/AthleticsViews/AthleticsView.swift
@@ -9,24 +9,9 @@ import SwiftUI
 import MapKit
 
 struct AthleticsView: View {
-    @State private var SelectedTeam = "Track & Field"
-
-
-    @State private var currentMapCoordinate = CLLocationCoordinate2D(latitude: 44.042216, longitude: -123.070818)
-    @State private var currentMapDistance = 400
-    @State private var currentMapPitch = 60
-    @State private var currentMapHeading = 200
     
     var body: some View {
-//        ScrollView{
-//            MapSatelliteView(coordinate: currentMapCoordinate, distance: CLLocationDistance(currentMapDistance), pitch: CGFloat(currentMapPitch), heading: CLLocationDirection(currentMapHeading), isSpinningEnabled: false)
-//                .frame(height: 400)
-//            
-//            DucksGamesView()
-//
-//        }
-//        .edgesIgnoringSafeArea([.top])
-        
+
         DucksGamesView()
 
     }
